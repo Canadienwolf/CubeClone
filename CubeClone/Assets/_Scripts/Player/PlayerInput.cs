018ab648b93c2db33c4becc1c0a94d168112b0b6
Hafrass <sindreva3@gmail.com>
2020-07-09 13:44:46 +0200

    Created scripts folder with wip player controller

---
+++ CubeClone/Assets/_Scripts/Player/PlayerInput.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput
+{
+    public Vector3 MoveDirection()
+    {
+        return new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+
+    public bool IsMoving()
+    {
+        return MoveDirection() != Vector3.zero;
+    }
+
+    public bool Jump(bool grounded)
+    {
+        return grounded && Input.GetKeyDown("space");
+    }
+
+    public bool Interact()
+    {
+        return Input.GetKeyDown("e");
+    }
+
+    public bool Attack()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+
+    public bool Block()
+    {
+        return Input.GetMouseButtonDown(1);
+    }
+}
